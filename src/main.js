c347b30d71e74d031ba54aabb32adff9ed62f5a6
yangjun <yangjun0506@icloud.com>
2021-05-18 15:57:39 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import router from "./router";
 
 // 设置 false 以阻止 Vue 在启动时生成生产提示  , true 提示 npm run build
 Vue.config.productionTip = false
@@ -7,5 +8,6 @@ Vue.config.productionTip = false
 
 // 创建一个新的 Vue 实例
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
